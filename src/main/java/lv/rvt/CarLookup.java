29ec6b4be5131e2f6421bc5333173c07d19fbd6b
A231986RO <A231986RO@rvt.lv>
2025-03-14 09:22:00 +0200

    Izveidots Car un Carlookup class, kur Carlookup ir atbildīgs par Car objektu izveidi.

---
+++ src/main/java/lv/rvt/CarLookup.java
@@ -0,0 +1,47 @@
+package lv.rvt;
+import java.io.*;
+import java.util.*;
+
+public class CarLookup {
+    private List<Car> cars;
+
+    public CarLookup(String filePath) {
+        this.cars = readCarsFromCSV(filePath);
+    }
+
+    private List<Car> readCarsFromCSV(String filePath) {
+        List<Car> cars = new ArrayList<>();
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                if (values.length == 5) {
+                    String marka = values[0].trim();
+                    String modelis = values[1].trim();
+                    String tips = values[2].trim();
+                    int gads = Integer.parseInt(values[3].trim());
+                    double stundasMaksa = Double.parseDouble(values[4].trim());
+                    cars.add(new Car(marka, modelis, tips, gads, stundasMaksa));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Kluda lasot CSV failu: " + e.getMessage());
+        }
+        return cars;
+    }
+
+
+    public List<Car> filterCarsByMake(String make) {
+        List<Car> result = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getMarka().equalsIgnoreCase(make)) {
+                result.add(car);
+            }
+        }
+        return result;
+    }
+}
+
+
